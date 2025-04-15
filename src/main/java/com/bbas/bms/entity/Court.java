0622fd765951c12e013e5d21e8039c2a6a5e40a8
dragon0622 <iamdragon.dev@gmail.com>
2025-04-15 20:14:48 +0900

    feat: 엔티티 추가(users, court, item)

---
+++ src/main/java/com/bbas/bms/entity/Court.java
@@ -0,0 +1,38 @@
+package com.bbas.bms.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Court {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "address", nullable = false, unique = true)
+    private String address;
+
+    @Column(name = "url", unique = true)
+    private String url;
+
+    @Column(name = "phone_num", unique = true)
+    private String phone_num;
+
+    @Column(name = "is_indoor", nullable = false)
+    private String is_indoor;
+
+    @Column(name = "open_time", nullable = false)
+    private String open_time;
+
+    @Column(name = "close_time", nullable = false)
+    private String close_time;
+}
