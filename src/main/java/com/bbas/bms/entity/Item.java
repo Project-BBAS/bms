8ed2c31fd61809210f4f71db047007cf555fcab0
pangvelop <kgo103@naver.com>
2025-06-02 21:05:31 +0900

    refactor: 수정

--- src/main/java/com/bbas/bms/entity/Item.java
+++ src/main/java/com/bbas/bms/entity/Item.java
@@ -2,9 +2,6 @@ package com.bbas.bms.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.NoArgsConstructor;
 
