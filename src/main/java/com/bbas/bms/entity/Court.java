4b066783296179afc56cebe3785a6428fe884b1f
김광오 <107342259+pangvelop@users.noreply.github.com>
2025-06-04 16:21:36 +0900

    풀 리퀘스트 병합 #4
    * refactor: BaseEntity 생성
    
    * refactor: 수정
    
    * refactor: 주석 추가, BaseEntity 수정

--- src/main/java/com/bbas/bms/entity/Court.java
+++ src/main/java/com/bbas/bms/entity/Court.java
@@ -2,18 +2,11 @@ package com.bbas.bms.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
-public class Court {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Court extends BaseEntity{
 
     @Column(name = "name", nullable = false, unique = true)
     private String name;
